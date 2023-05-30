369c9fa308c0bf49ac2c558160fea4f146f98c32
n17y17 <n17y17@xmailer.be>
2023-05-30 18:47:19 +0900

    ビット全探索の練習

---
+++ At_coder/arc/arc105/arc105_a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    vector<long> cookie(4);
+    long sum=0;
+    rep(i,4){
+        cin >> cookie.at(i);
+        sum += cookie.at(i);
+    }
+
+    long eat;
+
+    for(unsigned bit=0; bit<16; bit++){ //16=2^4
+        eat=0;
+        rep(i,4){
+            if(bit & (1U<<i)) eat += cookie.at(i);
+        }
+        if(eat == (sum-eat)){
+            cout << "Yes" << endl;
+            exit(0);
+        }
+    }
+    cout << "No" << endl;
+}
